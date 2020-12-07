1e9b5ea06b523668464258cc53ba4328036700f7
Markus Schäfer <github@ockenheimer.net>
2020-12-07 12:33:37 +0100

    day 5 finished

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import time
+
 start_time = time.time()
 
+# import dec01
+# # run dec01
+# dec01
 
+# import dec02
+# # run dec02
+# dec02
 
-#import dec01
-#run dec01
-#dec01
+# import dec03
+# # run dec03
+# dec03
+#
+# import dec04
+# # run dec04
+# dec04
 
-#import dec02
-#run dec02
-#dec02
-
-#import dec03
-#run dec03
-#dec03
-
-import dec04
-dec04
-
+import dec05
+dec05
 
 print("--- %s seconds ---" % (time.time() - start_time))
